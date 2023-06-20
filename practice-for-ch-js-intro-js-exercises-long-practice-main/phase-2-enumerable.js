8a0e4f9708c3a87e38b997a4a79d3f56884049c8
joshua <joshk.5793@gmail.com>
2023-06-20 15:06:47 -0400

    finished enumerables

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,36 @@
-// Your code here
+Array.prototype.myEach = function(callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i]);
+    }
+};
+
+// Array.prototype.myMap = function(callback) {
+//     let mappedArr = [];
+
+//     this.myEach(ele => {
+//         mappedArr.push(callback(ele))
+//     })
+//     return mappedArr;
+// }
+
+Array.prototype.myReduce = function(callback, initialValue) {
+
+    initialValue = initialValue || this[0]
+
+    if (initialValue === this[0]) {
+        let arr = this.slice(1)
+        arr.myEach(ele => {
+            initialValue = callback(initialValue, ele)
+        })
+    } else {
+        this.myEach(ele => {
+            initialValue = callback(initialValue, ele)
+        })
+    }
+    return initialValue;
+}
+
+
+console.log([1, 2, 3].myReduce(function(acc, el) {
+    return acc + el;
+  }, 2))
