e48820366c311fcacfaf57738019d17d8e89aa2f
root <somra45@gmail.com>
2023-06-20 16:19:47 -0400

    finish phase 3

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-3-iteration.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-3-iteration.js
@@ -1,1 +1,28 @@
-// Your code here
+Array.prototype.bubbleSort = function() {
+    let sorted = false;
+    while (!sorted) {
+        sorted = true;
+        for (let i = 0; i < this.length; i++) {
+            if (this[i] > this[i+1]) {
+                [this[i], this[i+1]] = [this[i+1], this[i]];
+                sorted = false;
+            }
+        }
+    }
+    return this;
+};
+
+// console.log([1, 16, 2, 8, 2 , 7, 6].bubbleSort())
+
+String.prototype.subStrings = function() {
+    let array = [];
+    for (let i = 0; i < this.length; i++) {
+        for (let j = i; j < this.length; j++) {
+            array.push(this.slice(i, j+1));
+        }
+    }
+    return array; 
+};
+
+// console.log('goth'.subStrings())
+
